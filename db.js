98c4c258d98acb53f37d0befd4a8da2c35e3bba6
hsrvms <59848940+hsrvms@users.noreply.github.com>
2023-11-27 14:10:27 +0300

    Revert "initial commit"
    This reverts commit fa0d3353ee9ae418b2e19f8a7c232ac68c43b731.

--- db.js
+++
@@ -1,8 +0,0 @@
-const { Pool } = require("pg");
-
-const pool = new Pool({
-	connectionString:
-		"postgres://cdmgtche:AYc6tQCegRwOc3IEx7Dx-FbhYXFBXIdj@berry.db.elephantsql.com/cdmgtche",
-});
-
-module.exports = pool;
